64e15a160e03a2a044952116890572701e96dc59
PriyanshuPaul <priyanshupaul23@gmail.com>
2024-09-22 01:52:22 +0530

    making initial adjustments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCrf5tx9SZcIEl_y8DbjcdyfcWpktxvAYs",
+    authDomain: "mykanban-d7d77.firebaseapp.com",
+    projectId: "mykanban-d7d77",
+    storageBucket: "mykanban-d7d77.appspot.com",
+    messagingSenderId: "1049405103529",
+    appId: "1:1049405103529:web:ec5e448e8676499a04b8b8",
+    production: false
+};
